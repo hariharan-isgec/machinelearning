5fc60c693552bcd3dbb2bebb78ca986a76e69e7b
Hariharan <hariharan@isgec.co.in>
2019-09-19 15:56:22 +0530

    Upgraded 5 Neuron to 8 Neuron
    Some cases were leftout so had to expand the Neural Network

--- tictactoe.py
+++ tictactoe.py
@@ -4,13 +4,13 @@ import numpy, random, os
 lr = 1 #learning rate part of NeuralNetwork Perceptron Algorithm
 bias = 1 #value of bias part of NeuralNetwork Perceptron Algorithm
 weights_2_neurons = [random.random(),random.random(),random.random()] #weight for 2 Neuron NeuralNetwork as per Perceptron Algorithm
-weights_5_neurons = [random.random(),random.random(),random.random(),random.random(),random.random(),random.random()] #weight for 5 Neuron NeuralNetwork as per Perceptron Algorithm
+weights_8_neurons = [random.random(),random.random(),random.random(),random.random(),random.random(),random.random(),random.random(),random.random(),random.random()] #weight for 8 Neuron NeuralNetwork as per Perceptron Algorithm
 
 ##### Creating Neural Network Generic Functions ##### 
 def ActivationFunction_2_nerons(input1, weights1, input2, weights2, bias, weights3) :
    outputP = input1*weights1+input2*weights2+bias*weights3
 
-   if outputP > 0 : #activation function (here Heaviside)
+   if outputP > 0 : #activation function (here Heaviside) True or False comes from Neurons getting trained
       outputP = 1
    else :
       outputP = 0
@@ -26,27 +26,30 @@ def Perceptron_2_nerons(input1, input2, output) :
    weights_2_neurons[2] += error * bias * lr
    
    
-def ActivationFunction_5_neurons(input1, weights1, input2, weights2,input3, weights3, input4, weights4, input5, weights5, bias, weights6) :
-   outputP = input1*weights1+input2*weights2+input3*weights3+input4*weights4+input5*weights5+bias*weights6
+def ActivationFunction_8_neurons(input1, weights1, input2, weights2,input3, weights3, input4, weights4, input5, weights5, input6, weights6, input7, weights7,input8, weights8, bias, weights9) :
+   outputP = input1*weights1+input2*weights2+input3*weights3+input4*weights4+input5*weights5+input6*weights6+input7*weights7+input8*weights8+bias*weights9
 
-   if outputP > 0 : #activation function (here Heaviside)
+   if outputP > 0 : #activation function (here Heaviside) True or False comes from Neurons getting trained
       outputP = 1
    else :
       outputP = 0
 	
    return outputP
 
-def Perceptron_5_neurons(input1, input2, input3, input4, input5,output) :
-   outputP = ActivationFunction_5_neurons(input1,weights_5_neurons[0],input2,weights_5_neurons[1],input3,weights_5_neurons[2],input4,weights_5_neurons[3],input5,weights_5_neurons[4],bias,weights_5_neurons[5])   
+def Perceptron_8_neurons(input1, input2, input3, input4, input5, input6, input7, input8,output) :
+   outputP = ActivationFunction_8_neurons(input1,weights_8_neurons[0],input2,weights_8_neurons[1],input3,weights_8_neurons[2],input4,weights_8_neurons[3],input5,weights_8_neurons[4],input6,weights_8_neurons[5],input7,weights_8_neurons[6],input8,weights_8_neurons[7],bias,weights_8_neurons[8])   
    
    error = output-outputP
-   weights_5_neurons[0] += error * input1 * lr
-   weights_5_neurons[1] += error * input2 * lr
-   weights_5_neurons[2] += error * input3 * lr
-   weights_5_neurons[3] += error * input4 * lr
-   weights_5_neurons[4] += error * input5 * lr
+   weights_8_neurons[0] += error * input1 * lr
+   weights_8_neurons[1] += error * input2 * lr
+   weights_8_neurons[2] += error * input3 * lr
+   weights_8_neurons[3] += error * input4 * lr
+   weights_8_neurons[4] += error * input5 * lr
+   weights_8_neurons[5] += error * input5 * lr
+   weights_8_neurons[6] += error * input5 * lr
+   weights_8_neurons[7] += error * input5 * lr
    
-   weights_5_neurons[5] += error * bias * lr
+   weights_8_neurons[8] += error * bias * lr
    #print("PostCorrection,",weights_5_neurons[0],",",weights_5_neurons[1],",",weights_5_neurons[2],",",weights_5_neurons[3],",",weights_5_neurons[4],",",weights_5_neurons[5],",",error)
    
 def Teach_2_neurons():   
@@ -57,24 +60,52 @@ def Teach_2_neurons():
 	   Perceptron_2_nerons(0,1,0)
 	   Perceptron_2_nerons(0,0,1)
 	   
-def Teach_5_neurons():
-	# Teaching the 5 Neuron Neural Network so that can judge GameOver  
-	for i in range(50) :
-	   Perceptron_5_neurons(1,0,0,0,0,1) 
-	   Perceptron_5_neurons(0,1,0,0,0,1) 
-	   Perceptron_5_neurons(0,0,1,0,0,1) 
-	   Perceptron_5_neurons(0,0,0,1,0,1) 
-	   Perceptron_5_neurons(0,0,0,0,1,1) 
-	   Perceptron_5_neurons(0,0,0,0,0,0) 
+def Teach_8_neurons():
+	# Teaching the 8 Neuron Neural Network so that can judge GameOver  
+	for i in range(100) :
+	   Perceptron_8_neurons(1,0,0,0,0,0,0,0,1) 
+	   Perceptron_8_neurons(0,1,0,0,0,0,0,0,1) 
+	   Perceptron_8_neurons(0,0,1,0,0,0,0,0,1) 
+	   Perceptron_8_neurons(0,0,0,1,0,0,0,0,1) 
+	   Perceptron_8_neurons(0,0,0,0,1,0,0,0,1) 
+	   Perceptron_8_neurons(1,0,0,0,0,1,0,0,1) 
+	   Perceptron_8_neurons(0,0,0,0,0,0,1,0,1) 
+	   Perceptron_8_neurons(0,0,0,0,0,0,0,1,1) 
+	   Perceptron_8_neurons(0,0,0,0,0,0,0,0,0) 
 
 ##### End of Generic Neural Functions ########## End of Generic Neural Functions #####
 
 ##### Main Body ########## Main Body #####
 # Speciifc Functions for Tic Tac Toe
-
+def AutoMode(board_row1,board_row2, board_row3):
+	position = 10
+	if GetEmptyCell(board_row1) > -1 :
+		print("AutoValue",position)
+		return position + GetEmptyCell(board_row1)
+	position = 20
+	if GetEmptyCell(board_row2) > -1 :
+		print("AutoValue",position)
+		return position + GetEmptyCell(board_row2)
+	position = 30
+	if GetEmptyCell(board_row3) > -1 :
+		print("AutoValue",position)
+		return position + GetEmptyCell(board_row3)
+	print("No Empty Cell")	
+	return 0	
+		
+def GetEmptyCell(board_row) :
+	print("Cell Value",board_row[0],board_row[1],board_row[2])
+	if board_row[0]>2:
+		return 0
+	if board_row[1]>2:
+		return 1
+	if board_row[2]>2:
+		return 2
+	return -1
+		
 def Check_Game_Status(board_row1,board_row2,board_row3):
 
-	# Check the Board Values to Pass it to Neural Network with 2 Neurons 
+	# Check the Board Values to Pass it to Neural Network with 2 Neurons the beauty is there is no IF conditions here
 	check_game_row1=[0,0]
 	check_game_row1[0]=abs(board_row1[0]-board_row1[1])
 	check_game_row1[1]=abs(board_row1[1]-board_row1[2])
@@ -84,6 +115,17 @@ def Check_Game_Status(board_row1,board_row2,board_row3):
 	check_game_row3=[0,0]
 	check_game_row3[0]=abs(board_row3[0]-board_row3[1])
 	check_game_row3[1]=abs(board_row3[1]-board_row3[2])
+	
+	check_game_down1=[0,0]
+	check_game_down1[0]=abs(board_row1[0]-board_row2[0])
+	check_game_down1[1]=abs(board_row2[0]-board_row3[0])
+	check_game_down2=[0,0]
+	check_game_down2[0]=abs(board_row1[1]-board_row2[1])
+	check_game_down2[1]=abs(board_row2[1]-board_row3[1])
+	check_game_down3=[0,0]
+	check_game_down3[0]=abs(board_row1[2]-board_row2[2])
+	check_game_down3[1]=abs(board_row2[2]-board_row3[2])
+
 	check_game_across1=[0,0]
 	check_game_across1[0]=abs(board_row1[0]-board_row2[1])
 	check_game_across1[1]=abs(board_row2[1]-board_row3[2])
@@ -94,11 +136,14 @@ def Check_Game_Status(board_row1,board_row2,board_row3):
 	game_status_row1 = ActivationFunction_2_nerons(check_game_row1[0], weights_2_neurons[0], check_game_row1[1], weights_2_neurons[1], bias, weights_2_neurons[2])
 	game_status_row2 = ActivationFunction_2_nerons(check_game_row2[0], weights_2_neurons[0], check_game_row2[1], weights_2_neurons[1], bias, weights_2_neurons[2])
 	game_status_row3 = ActivationFunction_2_nerons(check_game_row3[0], weights_2_neurons[0], check_game_row3[1], weights_2_neurons[1], bias, weights_2_neurons[2])
+	game_status_down1 = ActivationFunction_2_nerons(check_game_down1[0], weights_2_neurons[0], check_game_down1[1], weights_2_neurons[1], bias, weights_2_neurons[2])
+	game_status_down2 = ActivationFunction_2_nerons(check_game_down2[0], weights_2_neurons[0], check_game_down2[1], weights_2_neurons[1], bias, weights_2_neurons[2])
+	game_status_down3 = ActivationFunction_2_nerons(check_game_down3[0], weights_2_neurons[0], check_game_down3[1], weights_2_neurons[1], bias, weights_2_neurons[2])
 	game_status_across1 = ActivationFunction_2_nerons(check_game_across1[0], weights_2_neurons[0], check_game_across1[1], weights_2_neurons[1], bias, weights_2_neurons[2])
 	game_status_across2 = ActivationFunction_2_nerons(check_game_across2[0], weights_2_neurons[0], check_game_across2[1], weights_2_neurons[1], bias, weights_2_neurons[2])
 
-	# Pass the output of 2 Neuron Neural Network as Input to 5 Neurons Neural Network
-	FinalStatus = ActivationFunction_5_neurons(game_status_row1,weights_5_neurons[0],game_status_row2,weights_5_neurons[1],game_status_row3,weights_5_neurons[2],game_status_across1,weights_5_neurons[3],game_status_across2,weights_5_neurons[4],bias,weights_5_neurons[5])
+	# Pass the output of 2 Neuron Neural Network as Input to 8 Neurons Neural Network for Overall Status
+	FinalStatus = ActivationFunction_8_neurons(game_status_row1,weights_8_neurons[0],game_status_row2,weights_8_neurons[1],game_status_row3,weights_8_neurons[2],game_status_down1,weights_8_neurons[3],game_status_down2,weights_8_neurons[4],game_status_down3,weights_8_neurons[5],game_status_across1,weights_8_neurons[6],game_status_across2,weights_8_neurons[7],bias,weights_8_neurons[8])
 	return FinalStatus
 
 ################################
@@ -107,7 +152,7 @@ def Check_Game_Status(board_row1,board_row2,board_row3):
 ################################
 ################################	   
 Teach_2_neurons()
-Teach_5_neurons()
+Teach_8_neurons()
    
 # Tic Tac Board Status 
 board_row1 = [10,11,12]
@@ -115,6 +160,7 @@ board_row2 = [20,21,22]
 board_row3 = [30,31,32]
 FinalStatus=0
 PlayerNumber=2
+automode = 1
 position = 21
 while FinalStatus == 0 and position > 0:
 	if PlayerNumber == 1:
@@ -129,7 +175,15 @@ while FinalStatus == 0 and position > 0:
 	print("[", board_row3[0], "] [", board_row3[1], "] [", board_row3[2], "]")   
 	print("")
 	print("Input the Cell Number say 10,20, 21 etc or 0 to EXIT")
-	position=int(input())
+
+	if PlayerNumber == 1:
+		position=int(input())
+	else:
+		if automode == 0:
+			position=int(input())
+		else:
+			position=AutoMode(board_row1, board_row2, board_row3)
+			
 	if position == 10:
 		board_row1[0]=PlayerNumber
 	if position == 11:
@@ -152,6 +206,11 @@ while FinalStatus == 0 and position > 0:
 	FinalStatus = Check_Game_Status(board_row1,board_row2,board_row3)
 
 if FinalStatus == 1: 	
-	print("Game Won By:-", PlayerNumber)   
+	print("Game Won By Player:-", PlayerNumber)   
+	print("[", board_row1[0], "] [", board_row1[1], "] [", board_row1[2], "]")   
+	print("[", board_row2[0], "] [", board_row2[1], "] [", board_row2[2], "]")   
+	print("[", board_row3[0], "] [", board_row3[1], "] [", board_row3[2], "]")   
+	print("")
+
 else:
 	print("Game Draw !!!")   
